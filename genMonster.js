31f4fb14ac459580853b7b70b7063dd85ea19733
rispoliriccardo <rispoliriccardo@gmail.com>
2018-06-15 17:10:56 +0200

    genMonster and levelUpMonster functions

---
+++ genMonster.js
@@ -0,0 +1,16 @@
+module.exports = {
+  
+  genMonster: () => {
+    /*
+      Restituisce un oggetto con chiavi:
+      level (1), attackPower (1-20), defensePower (1-20), speed (1-20)
+    */
+  },
+
+  levelUpMonster: (monster) => {
+    /*
+      Restituisce il mostro livellato di 1 in un parametro casuale
+    */
+  }
+
+};
