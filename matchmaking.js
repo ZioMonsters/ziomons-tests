6c20fece864f14e3596a9030ea2049d0691681b3
Matteo Bonacini <matteo.bonna@gmail.com>
2018-06-15 18:26:09 +0200

    matchmaking_v0_working_2

--- matchmaking.js
+++ matchmaking.js
@@ -5,10 +5,9 @@ const { round } = Math;
 module.exports = () =>  {
   const t0 = genTeam();
   const l0 = round(t0.reduce((acc, {level}) => acc+level, 0)/5);
-  let t1;
   while (true) {
-    t1 = genTeam();
-    const l1 = round(t0.reduce((acc, {level}) => acc+level, 0)/5);
+    const t1 = genTeam();
+    const l1 = round(t1.reduce((acc, {level}) => acc+level, 0)/5);
     if (l1 === l0)
       return([t0, t1]);
   }
