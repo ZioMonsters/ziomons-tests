dea6f52810ad21399074b7b7d30f5e8bc92163cc
Emanuele <emanuele@localhost.localdomain>
2018-06-15 18:36:20 +0000

    stats

--- battle.js
+++ battle.js
@@ -1,11 +1,14 @@
 const matchmaking = require('./matchmaking.js'); //[[squadra1], [squadra2]]
 const fs = require('fs');
 
+let counter1 = 0, counter2 = 0;
+
   const battle = () => {
     const teams = matchmaking();
     const team1 = teams[0];
-    const team2 = team2[1];
+    const team2 = teams[1];
     const results = [];
+    let score1 = 0, score2 = 0;
 
     for(let i = 0; i<team1.length; i++){
       if(team1[i].speed > team2[i].speed){
@@ -24,15 +27,22 @@ const fs = require('fs');
         }
       }
     }
-
-  fs.writeFileSync('./data.txt', results, utf8);
   results.forEach((e) => {
-    if ( e === 1 ) score1++;
-    else if ( e === 2 ) score2++;
-  });
-  return (score1>score2)? score1:score2;
+      if ( e === 1 ) score1++;
+      else if ( e === 2 ) score2++;
+    });
+
+
+  const output = `${results.toString()} winner is: ${(score1>score2)? 1:2} percentage winning: ${(score1>score2)? score1/5*100:score2/5*100}%\n`
+  fs.appendFileSync('./data.txt', output);
+  if(score1>score2) counter1++
+  else counter2++;
   }
 
-for(let i = 0; i<30; i++){
+const matches = 400;
+
+for(let i = 0; i<matches; i++){
   battle();
 }
+
+fs.appendFileSync('./data.txt', `winning ${(counter1>counter2)? 1:2} in ${(counter1>counter2)? counter1/matches*100:counter2/matches*100}%`);
