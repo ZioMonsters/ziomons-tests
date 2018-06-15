015b9e593b96a9fc2115820fb5f98d2f8171a037
Matteo Bonacini <matteo.bonna@gmail.com>
2018-06-15 17:31:44 +0200

    mods

--- matchmaking.js
+++ matchmaking.js
@@ -1,5 +1,18 @@
 const genTeam = require('./genTeam.js');
 
+const { log } = console;
+
+//20 level zones
+const teams = [];
+
+for (let i = 0; i < 32767; i++) {
+  const t0 = genTeam();
+  const l0 = t0.reduce(({level}) => acc+level, 0)/5;
+  teams[l0/5].push(t0);
+}
+
+log(teams);
+log(teams.map(e => e.reduce(() => acc++, 0)));
 
 /*
 * returns array of arrays:
@@ -9,5 +22,9 @@ const genTeam = require('./genTeam.js');
 * ] //(See pokemon json format form genMonster.js
 * */
 module.exports = () => {
-
+  return teams[randInt(0, 20)].slice(0, 2);
 };
+
+const randInt = (min, max) => {
+  return Math.random() * (max - min) + min;
+}
