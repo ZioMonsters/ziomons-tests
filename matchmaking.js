8cc0be947f211069ae24b842d206940aebdc67cb
Matteo Bonacini <matteo.bonna@gmail.com>
2018-06-15 17:09:27 +0200

    mods

---
+++ matchmaking.js
@@ -0,0 +1,13 @@
+const genTeam = require('./genTeam.js');
+
+
+/*
+* returns array of arrays:
+* [
+*   [{(pokemon1)}, {(pokemon2)}, {(pokemon3)}, {(pokemon4)}],
+*   [{(pokemon1)}, {(pokemon2)}, {(pokemon3)}, {(pokemon4)}]
+* ] //(See pokemon json format form genMonster.js
+* */
+module.exports = () => {
+
+};
