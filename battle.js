664955835ef184d3121c503221092ca3000eb517
Emanuele <emanuele@localhost.localdomain>
2018-06-15 18:11:44 +0000

    mods

--- battle.js
+++ battle.js
@@ -1,13 +1,12 @@
 const matchmaking = require('./matchmaking.js'); //[[squadra1], [squadra2]]
 const fs = require('fs');
 
-module.exports = {
-
-  battle: () => {
+  const battle = () => {
     const teams = matchmaking();
     const team1 = teams[0];
     const team2 = team2[1];
     const results = [];
+
     for(let i = 0; i<team1.length; i++){
       if(team1[i].speed > team2[i].speed){
         if (team1[i].attackPower > team2[i].defensePower) results.push(1);
@@ -26,7 +25,14 @@ module.exports = {
       }
     }
 
-  //fs.writeFileSync('data.txt', )
+  fs.writeFileSync('./data.txt', results, utf8);
+  results.forEach((e) => {
+    if ( e === 1 ) score1++;
+    else if ( e === 2 ) score2++;
+  });
+  return (score1>score2)? score1:score2;
   }
 
+for(let i = 0; i<30; i++){
+  battle();
 }
