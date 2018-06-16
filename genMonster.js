7e04edb9e6f6266d1ff3f0f4c9f938d724489855
rispoliriccardo <rispoliriccardo@gmail.com>
2018-06-16 22:51:23 +0200

    genMonster.js mods 5

--- genMonster.js
+++ genMonster.js
@@ -10,20 +10,29 @@ module.exports = {
       speed: Math.floor((Math.random() * 100) + 1),
       rarity
     };
+    
     return monster;
     
     /** Restituisce un oggetto con chiavi: **/
     /** level (1-100), attackPower (1-100), defensePower (1-100), speed (1-100), rarity (1-4) **/
 
-  },
+  }
 
-  levelUpMonster: (monster) => {
-    // todo
-    
-    /** Restituisce il mostro livellato di 1 in un parametro casuale **/
-  
-  },
 };
+
+function statGenerator(level, rarity) {
+  const rarityModifiers = [0, 10, 20, 40];
+  if (level <=  10) return Math.floor((Math.random() * 6) + 1) + rarityModifiers[rarity - 1];
+  if (level <=  20) return Math.floor((Math.random() * 6) + 7) + rarityModifiers[rarity - 1];
+  if (level <=  30) return Math.floor((Math.random() * 6) + 13) + rarityModifiers[rarity - 1];
+  if (level <=  40) return Math.floor((Math.random() * 6) + 19) + rarityModifiers[rarity - 1];
+  if (level <=  50) return Math.floor((Math.random() * 6) + 25) + rarityModifiers[rarity - 1];
+  if (level <=  60) return Math.floor((Math.random() * 6) + 31) + rarityModifiers[rarity - 1];
+  if (level <=  70) return Math.floor((Math.random() * 6) + 37) + rarityModifiers[rarity - 1];
+  if (level <=  80) return Math.floor((Math.random() * 6) + 43) + rarityModifiers[rarity - 1];
+  if (level <=  90) return Math.floor((Math.random() * 6) + 49) + rarityModifiers[rarity - 1];
+  if (level <=  100) return Math.floor((Math.random() * 6) + 55) + rarityModifiers[rarity - 1];
+}
 
 /* -- Testing --
 for (let i = 0; i < 10; i++) {
